24e1765debdb5ef6dea29605131e1b81a92e35fd
jittagorn pitakmetagoon <jittagornp@gmail.com>
2015-03-10 10:46:44 +0700

    initial commit

---
+++ src/main/java/com/blogspot/na5cent/pyramid/Pyramid.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.blogspot.na5cent.pyramid;
+
+import java.util.Scanner;
+
+/**
+ * @author redcrow
+ */
+public class Pyramid {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int size = n * (n + 1) / 2;
+        int bigSpace = (n - 1) / 2;
+
+        for (int i = 1; i <= size; i++) {
+            int numb = i % n;
+            int space = bigSpace - i / n;
+
+            boolean star = numb > space && numb <= (n - space);
+            System.out.print(star ? "*" : " ");
+            System.out.print(numb == 0 ? "\n" : "");
+        }
+    }
+}
