a4ebec96cc2f531348641ee9b7437471db88916d
jittagorn pitakmetagoon <jittagornp@gmail.com>
2015-03-12 16:43:38 +0700

    add Example2

--- src/main/java/com/blogspot/na5cent/pyramid/Example2.java
+++ src/main/java/com/blogspot/na5cent/pyramid/Example2.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.blogspot.na5cent.pyramid;
 
 /**
- *
- * @author anonymous
+ * @author redcrow
  */
 public class Example2 {
 
